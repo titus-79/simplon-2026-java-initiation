dc93891209c022f21bb4db2dd5f3f803dafaeb8e
titus-79 <titus@ik.me>
2026-02-09 15:03:41 +0100

    feat: finish exercice 6

--- src/main/java/fr/simplon/exercises/Exercise06ArraysMaxMin.java
+++ src/main/java/fr/simplon/exercises/Exercise06ArraysMaxMin.java
@@ -3,57 +3,99 @@ package fr.simplon.exercises;
 /**
  * Exercice 6: Tableaux - Recherche du maximum et minimum
  * 
- * Objectif: Apprendre à trouver le plus grand et le plus petit élément d'un tableau
+ * Objectif: Apprendre à trouver le plus grand et le plus petit élément d'un
+ * tableau
  */
 public class Exercise06ArraysMaxMin {
-    
+
     /**
      * Trouve le maximum dans un tableau
+     * 
      * @param array le tableau
      * @return la valeur maximale
      */
     public int findMax(int[] array) {
-        throw new UnsupportedOperationException();
-
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] > max) {
+                max = array[i];
+            }
+        }
+        return max;
     }
-    
+
     /**
      * Trouve le minimum dans un tableau
+     * 
      * @param array le tableau
      * @return la valeur minimale
      */
     public int findMin(int[] array) {
-        throw new UnsupportedOperationException();
-
+        int min = Integer.MAX_VALUE;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] < min) {
+                min = array[i];
+            }
+        }
+        return min;
     }
-    
+
     /**
      * Trouve l'index du maximum dans un tableau
+     * 
      * @param array le tableau
      * @return l'index de la valeur maximale (le premier si plusieurs)
      */
     public int findMaxIndex(int[] array) {
-        throw new UnsupportedOperationException();
-
+        int max = Integer.MIN_VALUE;
+        int index = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] > max) {
+                max = array[i];
+                index = i;
+            }
+        }
+        return (index);
     }
-    
+
     /**
      * Trouve l'index du minimum dans un tableau
+     * 
      * @param array le tableau
      * @return l'index de la valeur minimale (le premier si plusieurs)
      */
     public int findMinIndex(int[] array) {
-        throw new UnsupportedOperationException();
-
+        int min = Integer.MAX_VALUE;
+        int index = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] < min) {
+                min = array[i];
+                index = i;
+            }
+        }
+        return (index);
     }
-    
+
     /**
      * Calcule la différence entre le max et le min
+     * 
      * @param array le tableau
      * @return la différence max - min
      */
     public int range(int[] array) {
-        throw new UnsupportedOperationException();
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] < min) {
+                min = array[i];
+            }
+            if (array[i] > max) {
+                max = array[i];
+            }
+        }
+
+        return max - min;
 
     }
 }
