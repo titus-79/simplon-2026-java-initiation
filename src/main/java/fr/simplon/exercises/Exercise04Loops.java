511a17375ddd1d074e07981539ae3a5f15001e5d
titus-79 <titus@ik.me>
2026-02-09 14:18:46 +0100

    feat: finish exercice 4

--- src/main/java/fr/simplon/exercises/Exercise04Loops.java
+++ src/main/java/fr/simplon/exercises/Exercise04Loops.java
@@ -6,54 +6,82 @@ package fr.simplon.exercises;
  * Objectif: Apprendre à utiliser les boucles for et while
  */
 public class Exercise04Loops {
-    
+
     /**
      * Calcule la somme des nombres de 1 à n
+     * 
      * @param n le nombre maximum
      * @return la somme de 1 + 2 + ... + n
      */
     public int sumUpToN(int n) {
-        throw new UnsupportedOperationException();
-
+        int somme = 0;
+        for (int i = 1; i <= n; i++) {
+            somme += i;
+        }
+        return somme;
     }
-    
+
     /**
      * Inverse un nombre (ex: 123 -> 321)
+     * 
      * @param number le nombre à inverser
      * @return le nombre inversé
      */
     public int reverseNumber(int number) {
-        throw new UnsupportedOperationException();
-
+        int result = 0;
+        while (number != 0) {
+            result = result * 10;
+            result = result + number % 10;
+            number = number / 10;
+        }
+        return result;
     }
-    
+
     /**
      * Calcule la factorielle d'un nombre (n!)
+     * 
      * @param n le nombre
      * @return n! (n factorielle)
      */
     public int factorial(int n) {
-        throw new UnsupportedOperationException();
-
+        int result = 1;
+        for (int i = 1; i <= n; i++) {
+            result = result * i;
+        }
+        return result;
     }
-    
+
     /**
      * Compte le nombre de chiffres dans un nombre
+     * 
      * @param number le nombre
      * @return le nombre de chiffres
      */
     public int countDigits(int number) {
-        throw new UnsupportedOperationException();
-
+        int count = 1;
+        while (number / 10 > 0) {
+            count ++;
+            number = number / 10;
+        }
+        return count;
     }
-    
+
     /**
      * Vérifie si un nombre est premier
+     * 
      * @param number le nombre à vérifier
      * @return true si le nombre est premier, false sinon
      */
     public boolean isPrime(int number) {
-        throw new UnsupportedOperationException();
+        if (number <= 1) {
+            return false;
+        }
+        for (int i = 2; i < number; i++){
+            if (number % i == 0) {
+                return false;
+            }
+        }
+        return true;
 
     }
 }
